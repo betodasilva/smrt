ec60a6648cfeadc7f455dcbdd3241da651047353
Thiago Franca <thiago@minify.com.br>
2019-10-02 18:58:54 -0300

    Instalação Angular

---
+++ ismart-ra2019/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+	title = 'ismart-ra2019';
+	// constructor(private router: Router){
+  	
+	// }
+}
