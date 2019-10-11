f074fc849a751c04a0676ebb5028607372e404e0
quevenrib <queven_rib@hotmail.com>
2019-10-11 17:21:00 -0300

    Pequenos ajustes

--- ismart-ra2019/src/app/services/testimonials/testimonials.service.ts
+++ ismart-ra2019/src/app/services/testimonials/testimonials.service.ts
@@ -11,8 +11,26 @@ export class TestimonialsService {
     {
       name: "Isabela Regina Julio de Moraes",
       excerpt: "18 anos, estudante do Colégio Objetivo, em São José dos Campos - SP",
-      testimonial: "",
-      photo: ""
+      testimonial: '"Conheci o projeto através de um amigo chamado Vinicius, que estudava na minha escola de origem e em uma das instituições parceiras do Ismart. Foi ele que meu incentivou e ajudou com todas as etapas do processo seletivo."',
+      photo: "assets/images/isabela-moraes.jpg"
+    },
+    {
+      name: "Maria Eduarda Machado",
+      excerpt: "Bolsista em Belo Horizonte – MG",
+      testimonial: '"Conheci o projeto através de um amigo chamado Vinicius, que estudava na minha escola de origem e em uma das instituições parceiras do Ismart. Foi ele que meu incentivou e ajudou com todas as etapas do processo seletivo."',
+      photo: "assets/images/placeholder.jpg"
+    },
+    {
+      name: "Pedro Cardoso Navarro",
+      excerpt: "Estudante de Engenharia Química na Universidade de Notre Dame, nos EUA",
+      testimonial: '"Conheci o projeto através de um amigo chamado Vinicius, que estudava na minha escola de origem e em uma das instituições parceiras do Ismart. Foi ele que meu incentivou e ajudou com todas as etapas do processo seletivo."',
+      photo: "assets/images/pedro-navarro.jpg"
+    },
+    {
+      name: "Maria Eduarda Machado",
+      excerpt: "Bolsista em Belo Horizonte – MG",
+      testimonial: '"Conheci o projeto através de um amigo chamado Vinicius, que estudava na minha escola de origem e em uma das instituições parceiras do Ismart. Foi ele que meu incentivou e ajudou com todas as etapas do processo seletivo."',
+      photo: "assets/images/isabela-moraes.jpg"
     }
   ]
 }
