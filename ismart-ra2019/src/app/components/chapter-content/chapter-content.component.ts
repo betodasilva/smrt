c822212247240832d889ab2f4b35e67ec5fddf6f
quevenrib <queven_rib@hotmail.com>
2019-10-14 15:16:03 -0300

    Share

--- ismart-ra2019/src/app/components/chapter-content/chapter-content.component.ts
+++ ismart-ra2019/src/app/components/chapter-content/chapter-content.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { WINDOW } from 'src/app/services/window.service';
+import { Injectable, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-chapter-content',
   templateUrl: './chapter-content.component.html',
   styleUrls: ['./chapter-content.component.scss']
 })
+
+@Injectable()
 export class ChapterContentComponent implements OnInit {
+  route: string;
+  constructor(@Inject(WINDOW) private window: Window) {
 
-  constructor() { }
-
+    // console.log(this.window.location.href);
+  }
+  getURL = this.window.location.href;
+  
   ngOnInit() {
+    // this.href = this.router.url;
+    // console.log(this.router);
   }
 
 }
