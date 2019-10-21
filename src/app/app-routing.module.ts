956958851d9d351d95f9c51570c5a977d1148c32
Beto <beto@minify.com.br>
2019-10-21 11:24:27 -0300

    Merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { TimelineComponent } from './pages/03-timeline/timeline.component';
 import { ParceirosComponent } from './pages/05-parceiros/parceiros.component';
 import { HorizonteComponent } from './pages/06-horizonte/horizonte.component';
 import { ManifestoComponent } from './pages/07-manifesto/manifesto.component';
+import { ResultadosComponent } from './pages/08-resultados/resultados.component';
 import { ListaParceirosComponent } from './pages/09-lista-parceiros/lista-parceiros.component';
 import { ExpedienteComponent } from './pages/10-expediente/expediente.component';
 
@@ -24,7 +25,8 @@ const routes: Routes = [
   { path: '06-horizonte', component: HorizonteComponent },
   { path: '07-manifesto', component: ManifestoComponent },
   { path: '09-lista-parceiros', component: ListaParceirosComponent},
-  { path: '10-expediente', component: ExpedienteComponent}
+  { path: '10-expediente', component: ExpedienteComponent},
+  { path: '08-resultados', component: ResultadosComponent }
   // { path: '**', component: PageNotFoundComponent }
 ];
 
