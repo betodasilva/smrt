060c2f13cebc329e85f6b62c2820e2768745ad73
Beto <beto@minify.com.br>
2019-10-21 11:20:20 -0300

    Adiciona suporte para imagem mobile

--- src/app/components/chapter-hat/chapter-hat.component.ts
+++ src/app/components/chapter-hat/chapter-hat.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject, HostListener } from '@angular/core';
+import { WINDOW } from 'src/app/services/window.service';
+import { Subject } from 'rxjs';
+import { throttleTime } from 'rxjs/operators';
+
 
 interface chapterData {
   image,
+  imageMobile,
   title,
   subtitle
 }
@@ -14,11 +19,42 @@ interface chapterData {
 export class ChapterHatComponent implements OnInit {
 
   @Input('data') data: chapterData;
-  
-  constructor() { }
+  private windowWidth: any;
+  private resize$: Subject<any> = new Subject();
+
+  constructor(@Inject(WINDOW) private window: Window,) { }
 
   ngOnInit() {
-    
+    this.resize$
+      .asObservable()
+      .pipe(
+        throttleTime(500)
+      )
+      .subscribe( innerWidth => {
+        this.windowWidth = innerWidth
+      });
   }
+
+  get backgroundImage(){
+    let styles = {}
+    const isMobile = this.window.innerWidth < 768;
+    if ( isMobile ) {
+      styles = {
+        'background-image': `url(${this.data.imageMobile})`
+      }
+    } else {
+      styles = {
+        'background-image': `url(${this.data.image})`
+      }
+    }
+
+    return styles;
+  }
+
+  @HostListener('window:resize', ['$event.target'])
+  onResize(target) {
+    this.resize$.next(target.innerWidth);
+  }
+
 
 }
