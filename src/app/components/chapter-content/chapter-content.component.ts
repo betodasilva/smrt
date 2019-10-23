c733e99f64e75f939f390717480f5d6ff5d6b065
Beto <beto@minify.com.br>
2019-10-23 14:25:47 -0300

    Adiciona novo share em mensagem do conselho

--- src/app/components/chapter-content/chapter-content.component.ts
+++ src/app/components/chapter-content/chapter-content.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input, ViewChild, ElementRef, Renderer2, HostListener } from '@angular/core';
 import { WINDOW } from 'src/app/services/window.service';
 import { Injectable, Inject } from '@angular/core';
-import { Subject, Subscription } from 'rxjs';
-import { EmitterService } from 'src/app/services/emitter/emitter.service';
+
+
 
 @Component({
   selector: 'app-chapter-content',
@@ -13,109 +13,21 @@ import { EmitterService } from 'src/app/services/emitter/emitter.service';
 @Injectable()
 export class ChapterContentComponent implements OnInit {
 
-  @ViewChild('articleContainer', {static: true}) articleContainer: ElementRef;
-  @ViewChild('floatContainer', {static: true}) floatContainer: ElementRef;
-  private floatContainerStartPos: number;
-  private floatContainerEndPos: number;
-  private isFloatContainerFixed: boolean = false;
-  
-  private subscription: Subscription;
+  @ViewChild('article', {static: true}) article: ElementRef;
+  @ViewChild('share', {static: true, read: ElementRef}) shareComponent: ElementRef;
 
   constructor(
-    @Inject(WINDOW) private window: Window,
-    private renderer: Renderer2,
+    @Inject(WINDOW) private window: Window
   ) {}
 
   ngOnInit() {
-    this.floatContainerStartPos = this.floatContainer.nativeElement.offsetTop;
-    EmitterService.get('headerOpen').subscribe( isOpen => {
-      if ( isOpen ) {
-        this.renderer.setStyle(
-          this.floatContainer.nativeElement,
-          'transform',
-          'translateY(100px)'
-        )
-      } else {
-        this.renderer.removeStyle(
-          this.floatContainer.nativeElement,
-          'transform'
-        )
-      }
-    })
   }
 
   ngAfterViewInit(){
-    setTimeout(() => {
-      this.floatContainerEndPos = this.articleContainer.nativeElement.offsetHeight - 220;
-
-    }, 2000);
   }
 
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll(event){
-    const currentScroll = this.window.pageYOffset;
 
-    if ( currentScroll >= this.floatContainerStartPos ) {
-      this.isFloatContainerFixed = true;
-    } else{
-      this.isFloatContainerFixed = false; 
-    }
 
-    if ( currentScroll + 100 >= this.floatContainerEndPos && this.floatContainerEndPos ) {
-      this.setAbsolutePosition();
-    } else {
-      this.removeAbsolutePosition();
-    }
 
-  }
-
-  setAbsolutePosition(){
-    this.renderer.setStyle(
-      this.floatContainer.nativeElement,
-      'transition',
-      `none`,
-    )
-    this.renderer.setStyle(
-      this.floatContainer.nativeElement,
-      'top',
-      `${this.floatContainerEndPos}px`,
-    )
-    this.renderer.setStyle(
-      this.floatContainer.nativeElement,
-      'position',
-      'absolute',
-    )
-    this.renderer.setStyle(
-      this.floatContainer.nativeElement,
-      'width',
-      '100%',
-    )
-    this.renderer.removeStyle(
-      this.floatContainer.nativeElement,
-      'transform'
-    )
-  }
-
-  removeAbsolutePosition(){
-    setTimeout( () => {
-      this.renderer.removeStyle(
-        this.floatContainer.nativeElement,
-        'transition'
-      )
-    }, 200)
-    
-    this.renderer.removeStyle(
-      this.floatContainer.nativeElement,
-      'top'
-    )
-    this.renderer.removeStyle(
-      this.floatContainer.nativeElement,
-      'position'
-    )
-    this.renderer.removeStyle(
-      this.floatContainer.nativeElement,
-      'width'
-    )
-  }
 
 }
