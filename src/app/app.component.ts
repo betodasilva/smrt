0a0c9e8be4b7bc266faced9257a2c38fcc0cb47c
Beto <beto@minify.com.br>
2019-10-29 12:14:39 -0300

    Teste bugfix para calculo da rolagem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Renderer2 } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,12 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	constructor(private translate: TranslateService, private router: Router){
+	constructor(
+        private translate: TranslateService, 
+        private router: Router,
+        @Inject(DOCUMENT) private document: Document,
+        private renderer: Renderer2
+    ){
 		translate.addLangs(['pt-BR', 'en'])
 		translate.setDefaultLang('pt-BR');
 		
@@ -22,5 +28,39 @@ export class AppComponent {
             }
             window.scrollTo(0, 0)
         });
+
+        
+    }
+
+    onRouterOutletActivate($event){
+        const chapterFigureEl = this.document.querySelectorAll('.chapter-content__img');
+        
+        chapterFigureEl.forEach( (el) => {
+            const imgContainer = el.querySelector('.img__container'),
+                  imgSrc       = el.querySelector('img').getAttribute('src'),
+                  img          = new Image();
+            
+            
+            img.src = imgSrc;
+            
+            
+            const interval = setInterval( () => {
+                if ( img.naturalWidth ) {
+                    // console.log( img.offsetWidth );
+                    clearInterval( interval );
+                    this.renderer.setStyle(
+                        imgContainer,
+                        'padding-bottom',
+                        `${(img.naturalHeight / img.naturalWidth) * 100}%`
+                    )
+                    // this.renderer.setStyle(
+                    //     el,
+                    //     'height',
+                    //     `${img.naturalHeight}px`
+                    // )
+                }
+            }, 10)
+            
+        })
     }
 }
